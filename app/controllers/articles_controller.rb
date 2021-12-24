485ffad264295841d1da2ad3ea156667024230a9
Karan Mehta <karanm645@gmail.com>
2021-12-24 17:39:54 -0600

    Create New Article Method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,23 @@ class ArticlesController < ApplicationController
   def new  
   end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.permit(:title, :body)    
   end
 end 
