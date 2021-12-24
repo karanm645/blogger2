d0dae0dc65864855517f2c52a476055b017af72d
Karan Mehta <karanm645@gmail.com>
2021-12-24 17:55:52 -0600

    Delete function

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Article Show Page" do 
   before :each do 
     @article_1 = Article.create!(title: "Title 1", body: "Body 1")
+    @article_2 = Article.create!(title: "Title 2", body: "Body 2")
   end 
 
   describe "they link from the articles index" do 
@@ -12,6 +13,17 @@ RSpec.describe "Article Show Page" do
 
       expect(page).to have_content(@article_1.title)
       expect(page).to have_content(@article_1.body)
+    end 
+  end 
+
+  describe "user can delete article" do 
+    it "they link from the show page" do 
+      visit article_path(@article_1)
+      click_link "Delete"
+
+      expect(current_path).to eq(articles_path)
+      expect(page).to have_content(@article_2.title)
+      expect(page).to_not have_content(@article_1.title)
     end 
   end 
 end 
